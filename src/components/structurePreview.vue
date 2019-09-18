6e15a6f62b8dbaf50de9b1350b262d2d3cf7fa65
Matthias Müller <moeli@moeli.com>
2019-09-18 12:11:18 +0200

    structure preview

---
+++ src/components/structurePreview.vue
@@ -0,0 +1,9 @@
+<script>
+    export default {
+        extends: 'k-files-field-preview',
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import clipHandle from "./components/clipHandle.vue";
 import clipListItem from "./components/clipListItem.vue";
 import clipButton from "./components/clipButton.vue";
 import clipDialog from "./components/clipDialog.vue";
+import structurePreview from "./components/structurePreview.vue";
 
 panel.plugin("mullema/image-clip", {
     fields: {
@@ -14,6 +15,7 @@ panel.plugin("mullema/image-clip", {
         'k-clip-handle': clipHandle,
         'k-clip-list-item': clipListItem,
         'k-clip-button': clipButton,
-        'k-clip-dialog': clipDialog
+        'k-clip-dialog': clipDialog,
+        'k-image-clip-field-preview': structurePreview
     }
 });
